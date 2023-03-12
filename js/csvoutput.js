173430fbabb653995146037a9c5d7b91adeb0e6f
fuyumikan <mtatsumi40@gmail.com>
2023-03-12 17:03:50 +0900

    csv内容を修正のためurlurl変更

--- js/csvoutput.js
+++ js/csvoutput.js
@@ -2,7 +2,7 @@ window.onload =function(){
     //出力場所を探す
     let output =document.getElementById("output");
     //CSVの指定
-    getCsv('https://rawcdn.githack.com/YuMake-Inc/saku-saku/main/csvdata/data.csv?token=GHSAT0AAAAAAB7NR7OGT5WHJPH7OX27THPSZANDWEA');
+    getCsv('https://rawcdn.githack.com/YuMake-Inc/saku-saku/main/csvdata/data.csv?token=GHSAT0AAAAAAB7NR7OGFWTQPYQO3QYATAQAZANQ5HA');
             
     //CSVの取り込み
     function getCsv(data){
--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ var sakuraIcon = L.icon({
 	
 	map.panTo(new L.LatLng(34.60438909041995, 135.83959579467776));
 
-  var csv0 = omnivore.csv('https://rawcdn.githack.com/YuMake-Inc/saku-saku/main/csvdata/data.csv?token=GHSAT0AAAAAAB7NR7OGT5WHJPH7OX27THPSZANDWEA',
+  var csv0 = omnivore.csv('https://rawcdn.githack.com/YuMake-Inc/saku-saku/main/csvdata/data.csv?token=GHSAT0AAAAAAB7NR7OGFWTQPYQO3QYATAQAZANQ5HA',
   {latfield:'lat',lonfield:'lng',delimiter:','},
   L.geoJson(null,
   {pointToLayer:function(feature,latlng)
